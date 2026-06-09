8cf694feeb325f2d24da776ffad61b9a53d6418e
GPi98 <pirosogiuseppe@gmail.com>
2026-06-09 11:08:40 +0200

    Add files via upload

---
+++ 02_Martedì09/Esempi_Clausole.py
@@ -0,0 +1,12 @@
+for i in range (5):
+    
+    if i == 3:
+        pass
+    print(i)
+    if i == 4:
+        continue
+    if i == 5:
+        break
+    
+numeri = [*range(1, 11)]
+print(numeri)
