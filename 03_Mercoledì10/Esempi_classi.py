e3d151646adf6c74466097c2d488ad4ae56c0188
GPi98 <pirosogiuseppe@gmail.com>
2026-06-10 12:58:19 +0200

    Add files via upload

---
+++ 03_Mercoledì10/Esempi_classi.py
@@ -0,0 +1,57 @@
+class Automobile:                   #dichiaro la classe
+    
+    numero_ruote = 4                #attributo di classe
+    
+    def __init__(self, marca, modello):     #metodo costruttore
+        
+        self.marca = marca                  #attributo di istanza
+        
+        self.modello = modello              #attributo di istanza
+        
+    def stampa_info(self):                  #metodo di istanza
+        
+        print("l'automobile è una ", self.marca, self.modello )
+
+auto1 = Automobile("Fiat", "500")       #crea un oggetto di Automobile
+auto2 = Automobile("BMW", "X3")
+
+auto1.stampa_info()                     #stampa L'Automobile è una Fiat 500
+auto2.stampa_info()                     #stampa L'Automobile è una BMW X3
+
+class Persona:
+    def __init__(self, nome, eta):
+        self.nome = nome                #attributo di istanza
+        self.eta = eta
+#creazione di p tipo Persona        
+p = Persona("Pippo",   30)
+
+print(p.nome)       #scrive Pippo
+print(p.eta)        #Scrive 30
+
+class Calcolatrice:
+    
+    @staticmethod
+    def somma(a,b):
+        return a + b
+    
+#uso del metodo statico senza creare istanza
+ristultato = Calcolatrice.somma(5, 3)
+
+print(risultato)
+#output 8
+
+class Contaore:
+    numero_istanze = 0  #attributo di classe
+    
+    def __init__(self):
+        Contatore.numero_istanza += 1
+    
+    @classmethod
+    def mostra_numero_istanze(cls):
+        print(f"Sono state create {cls.numero_istanze} istanze.")
+#creazioni di alcune istanze        
+c1 = Contatore()
+c2 = Contatore()
+
+Contatore.mostra_numero_istanze()
+#Sono state create 2 istanze
---
+++ 03_Mercoledì10/Esercizio_Classe_mattina.py
@@ -0,0 +1,36 @@
+class Punto:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def modifica_punto(self, n_x, n_y):
+        self.x = n_x
+        self.y = n_y
+        
+    def distanza_da_origine(self):
+        
+        distanza_x =  self.x
+        distanza_y =  self.y
+        
+        return distanza_x, distanza_y
+    
+
+    
+punto1 = Punto(1,2)         #creazione oggetto
+
+print(punto1.distanza_da_origine())
+
+class Libro:
+    
+    def __init__(self, nome, autore, pagine):
+        self.nome = nome
+        self.autore = autore
+        self.pagine = pagine
+    
+    def descrizione(self):
+        return "Il libro ", self.titolo," è stato scritto da", self.autore," e ha ", self.pagine, "pagine"
+        
+    
+libro1 = Libro("Il Signore degli Anell", "J. R. R. Tolkien", 1380)
+print(libro1.descrizione())
